22991ce980d373beaf3eb97315438b84048ea0f2
harrysmart15 <harikarthik508@gmail.com>
2023-01-26 10:44:27 +0530

    firstcommit

---
+++ array.js
@@ -0,0 +1,27 @@
+alert("sri");
+console.log('harry samrt');
+confirm("thgamey sry d innimel nan after noon thuga maten ok va...sry d pondati.luv u chlm");
+//prompt("Enter your coupe nick name");
+//var a =prompt("women name");
+//alert("weds");
+//var b =prompt("men name");
+//alert("srinidhi weds harikarthi");
+
+//prompt("match date");
+//var a = prompt("sydney");
+//var b = prompt("melbourne renegates")
+//alert("sydney");
+
+//var a = prompt("Enter your first name");
+//var b = prompt("Enter your second name");
+//alert(parseInt(a) + parseInt(b));
+
+
+//a=[12,3,4,5];
+//console.log(a.map((i)=>i%2==0));
+a=[1,2,3,4,5,6]
+for(let i of a){
+if (i%2){
+}
+console.log(i);
+}
---
+++ objectshorthand.js
@@ -0,0 +1,25 @@
+//const age =20;
+//const batch = "b43wd";
+//const mark = 90;
+
+//const student = {
+ //"srinidhi",
+   // age,
+    //batch,
+    //mark
+//};
+//console.log(student);
+
+
+let person ={
+firstname = "hari",
+lastname ="karthi",
+
+};
+
+let firstname = person.firstname;
+let lastname = person.lastname;
+
+let (firstname,lastname)=person;
+console.log(firstname);
+console.log(lastname);
